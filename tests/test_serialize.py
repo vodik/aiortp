b569a8162860546bbcd9c677fb9f1e9e8b5bbd27
Simon Gomizelj <simon@vodik.xyz>
2017-08-03 18:49:12 -0400

    Cleanup test formatting

--- tests/test_serialize.py
+++ tests/test_serialize.py
@@ -6,17 +6,19 @@ from aiortp.packet import rtpevent, pack_rtpevent, parse_rtpevent
 
 @given(binary(min_size=rtphdr.size, max_size=rtphdr.size + 1000))
 def test_rtp_decode_inverts_encode(pkt):
-    assert pack_rtp(parse_rtp(pkt)) == pkt
+    rtp = parse_rtp(pkt)
+    assert pack_rtp(rtp) == pkt
 
 
 @given(binary(min_size=rtpevent.size, max_size=rtpevent.size))
 def test_rtpevent_decode_inverts_encode(pkt):
-    assert pack_rtpevent(parse_rtpevent(pkt)) == pkt
+    rtpevent = parse_rtpevent(pkt)
+    assert pack_rtpevent(rtpevent) == pkt
 
 
 @given(binary(min_size=rtphdr.size + rtpevent.size,
               max_size=rtphdr.size + rtpevent.size))
-def test_rtp_and_rtpevent_decode_inverts_encode(pkt):
+def test_rtpevent_inside_rtp_decode_inverts_encode(pkt):
     rtp = parse_rtp(pkt)
     rtpevent = parse_rtpevent(rtp.pop('payload'))
 
