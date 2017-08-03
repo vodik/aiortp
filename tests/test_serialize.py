ddfc22bd15bba66eef0dcfe762db0b00ff307b11
Simon Gomizelj <simon@vodik.xyz>
2017-08-03 16:15:32 -0400

    Digit seperator is new in 3.4

--- tests/test_serialize.py
+++ tests/test_serialize.py
@@ -4,7 +4,7 @@ from aiortp.packet import rtphdr, pack_rtp, parse_rtp
 from aiortp.packet import rtpevent, pack_rtpevent, parse_rtpevent
 
 
-@given(binary(min_size=rtphdr.size, max_size=rtphdr.size + 1_000))
+@given(binary(min_size=rtphdr.size, max_size=rtphdr.size + 1000))
 def test_rtp_decode_inverts_encode(pkt):
     assert pack_rtp(parse_rtp(pkt)) == pkt
 
