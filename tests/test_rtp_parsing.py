791816e299756db240c35287b4d061cbcefd1485
Simon Gomizelj <simon@vodik.xyz>
2017-08-03 14:38:10 -0400

    Fix the decode/encode test

--- tests/test_rtp_parsing.py
+++ tests/test_rtp_parsing.py
@@ -1,8 +1,8 @@
 from hypothesis import given
 from hypothesis.strategies import binary
-from aiortp import *
+from aiortp.packet import rtphdr, pack_rtp, parse_rtp
 
 
-@given(binary(min_size=rtphdr.size, max_size=rtphdr.size))
-def test_decode_inverts_encode(pkt):
+@given(binary(min_size=rtphdr.size, max_size=rtphdr.size + 1_000))
+def test_rtp_decode_inverts_encode(pkt):
     assert pack_rtp(parse_rtp(pkt)) == pkt
