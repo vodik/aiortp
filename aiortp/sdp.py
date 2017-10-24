26c1daab6095c6110995104b94ad5b6260557c70
Simon Gomizelj <simon@vodik.xyz>
2017-10-23 20:50:47 -0400

    Remove python 3.6 only format strings

--- aiortp/sdp.py
+++ aiortp/sdp.py
@@ -3,22 +3,24 @@ class SDP:
         self.local_addr = local_addr
         self.ptime = ptime
 
-        local_addr_desc = f'IN IP4 {self.local_addr[0]}'
+        local_addr_desc = 'IN IP4 {}'.format(self.local_addr[0])
         self.payload = '\r\n'.join([
             'v=0',
-            f'o=user1 53655765 2353687637 {local_addr_desc}',
+            'o=user1 53655765 2353687637 {local_addr_desc}',
             's=-',
             't=0 0',
             'i=aiortp media stream',
-            f'm=audio {self.local_addr[1]} RTP/AVP 0 101 13',
-            f'c={local_addr_desc}',
+            'm=audio {local_port} RTP/AVP 0 101 13',
+            'c={local_addr_desc}',
             'a=rtpmap:0 PCMU/8000/1',
             'a=rtpmap:101 telephone-event/8000',
             'a=fmtp:101 0-15',
-            f'a=ptime:{self.ptime}',
+            'a=ptime:{ptime}',
             'a=sendrecv',
             '',
-        ])
+        ]).format(local_addr_desc=local_addr_desc,
+                  local_port=self.local_addr[1],
+                  ptime=self.ptime)
 
     def __str__(self):
         return self.payload
